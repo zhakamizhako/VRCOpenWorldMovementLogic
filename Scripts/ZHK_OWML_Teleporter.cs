1e8449f4d3218689d30f59c1edf55de06b7f57e0
ZZ <zhintamizhakami@gmail.com>
2022-10-09 11:43:49 +0800

    Added Teleporter Script

---
+++ Scripts/ZHK_OWML_Teleporter.cs
@@ -0,0 +1,19 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ZHK_OWML_Teleporter : UdonSharpBehaviour
+{
+    public ZHK_UIScript UIScript;
+    public Transform TargetLocation;
+    public Vector3 offsetPos;
+
+    public override void Interact()
+    {
+        Networking.LocalPlayer.TeleportTo(new Vector3(0,0,0), Networking.LocalPlayer.GetRotation());
+        UIScript.Map.position = UIScript.Map.position - TargetLocation.position + offsetPos;
+        Networking.LocalPlayer.SetVelocity(Vector3.zero);
+    }
+}
